5dac84657c1b606915a879039e9a16c37230b744
Aadesh Bagmar <aadesh@umd.edu>
2019-11-12 18:13:27 -0500

    Testing out things with Pandas

--- parser/parse.py
+++ parser/parse.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 
 def extract_params(data, param_list):
@@ -10,19 +11,18 @@ def extract_params(data, param_list):
     return return_object
 
 
-def parse():
-    with open("../data_collector/checkpoint/meta_package_data_0.json", "r") as f:
-        data = json.load(f)
+def parse(data):
 
     required_params = ["author_email", "maintainer_email", "name", "requires_dist", "license", "version"]
-
     key_splitters = ["(", "[", ";", "extra", "pytest"]
+    parsed_info = []
 
     for d in data:
         extracted_info = extract_params(d.get("info", {}).get("info", {}), required_params)
         dependencies = []
 
         if extracted_info.get("requires_dist") is None:
+            parsed_info.append(cleanup(extracted_info))
             continue
 
         for dependency in extracted_info.get("requires_dist"):
@@ -35,10 +35,32 @@ def parse():
             if dependency:
                 dependencies.append(dependency)
 
-        print(extracted_info.get("requires_dist"))
-        print(dependencies)
-        print("----------")
+        extracted_info["dependencies"] = dependencies
+        parsed_info.append(cleanup(extracted_info))
+
+    return parsed_info
+
+
+def cleanup(data):
+    keys = ["author_email", "maintainer_email", "name",  "license", "version", "dependencies"]
+
+    if data.get("author_email") is None or data.get("author_email").strip() is "":
+        data["author_email"] = data["maintainer_email"] if data.get("maintainer_email") else "UNKNOWN"
+
+    if data.get("maintainer_email") is None or data.get("maintainer_email").strip() is "":
+        data["maintainer_email"] = data["author_email"] if data.get("author_email") else "UNKNOWN"
+
+    for key in keys:
+        data[key] = data[key] if data.get(key) else "UNKNOWN"
+
+    del data["requires_dist"]
+    return data
 
 
 if __name__ == '__main__':
-    parse()
+    with open("../data_collector/checkpoint/meta_package_data_2000.json", "r") as f:
+        data = json.load(f)
+
+    df = pd.DataFrame(parse(data))
+    print(df.author_email.value_counts())
+
